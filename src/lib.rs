c02954c7124559af884239304b16c926b25196d7
German Molina <germolinal@gmail.com>
2022-06-16 09:25:05 +1200

    improved TimeSeries

--- src/lib.rs
+++ src/lib.rs
@@ -20,36 +20,35 @@ SOFTWARE.
 
 #![deny(missing_docs)]
 
-
 //! This crate was developed with the intent of helping you to validate scientific
 //! tools; for exmple, for comparing the results of the temperature calculated
 //! by an algorithm and those measured in an experiment. It is supposed to be embedded
 //! the unit or integration testing.
-//! 
+//!
 //! Different Validation tasks can be created by implementing the [`Validate`] trait.  
-//! These tasks will be stored into a [`Validations`] object which will write a report 
+//! These tasks will be stored into a [`Validations`] object which will write a report
 //! into a Markdown File.
-//! 
+//!
 //! # Example
-//! 
+//!
 //! ```
 //! use validate::{Validations, Validate};
 //! use std::{fs::File, io::Write};
-//! 
+//!
 //! // This checks that two numbers are equal
 //! struct CustomValidation {
 //!     expected: u8,
 //!     found: u8,
 //!     title: &'static str,
 //! }
-//! 
+//!
 //! impl Validate for CustomValidation {
 //!     fn validate(&self, file: &mut File)->Result<(),String>{
 //!         let valid = self.expected == self.found;
 //!         
 //!         
 //!         // We return an error if the validation is not succesful, but we still
-//!         // write something into the report. Even if this particular Validation 
+//!         // write something into the report. Even if this particular Validation
 //!         // fails, the Validations object will run all the validations and print
 //!         // the error messages into the STDERR
 //!         let (ret, buf) = if valid {
@@ -65,7 +64,7 @@ SOFTWARE.
 //!         ret
 //!     }
 //! }
-//! 
+//!
 //! let v = CustomValidation{
 //!     expected: 2,
 //!     found: 2,
@@ -74,24 +73,22 @@ SOFTWARE.
 //! let mut validations = Validations::new("report.md", ".");
 //! validations.push(Box::new(v));
 //! validations.validate().unwrap()
-//! 
+//!
 //! ```
-
 
 use std::{fs::File, io::Write};
 
-
-/// A Validator that plots two time series and calculates—if required—the 
+/// A Validator that plots two time series and calculates—if required—the
 /// Root Mean Squared Error and Mean Bias Error between them.
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// use validate::{Validations, SeriesValidator};
-/// 
+///
 /// let expected = vec![1., 2., 3.];
 /// let found = vec![5., 6., 6.];
-/// 
+///
 /// let mut validator = Validations::new("report.md", ".");    
 /// // Note that we are not defining a maximum allowed error
 /// let v = validate::SeriesValidator {
@@ -105,20 +102,18 @@ use std::{fs::File, io::Write};
 /// };
 /// validator.push(Box::new(v));
 ///
-/// // This will not fail because we did not set a maximum allowed 
-/// // Root Mean Square Error or Mean Bias Error... if we did, it would return 
+/// // This will not fail because we did not set a maximum allowed
+/// // Root Mean Square Error or Mean Bias Error... if we did, it would return
 /// // an error and the unwrap woul
 /// validator.validate().unwrap();
 /// ```
-
-
 mod time_series;
 pub use time_series::SeriesValidator;
 
 /// Asserts whether two numbers are close enough
-/// by comparing the first argument with the second, and 
+/// by comparing the first argument with the second, and
 /// the threshold being the third.
-/// 
+///
 /// # Examples
 /// ```
 /// use validate::assert_close;
@@ -131,8 +126,11 @@ macro_rules! assert_close {
         match ($left, $right, $allowed_diff) {
             (left_val, right_val, allowed_diff) => {
                 let diff = (left_val as f64 - right_val as f64).abs();
-                if (diff > allowed_diff) {                                        
-                    panic!("{} and {} are not close enough (allowed difference was {}... found {})", left_val, right_val, allowed_diff, diff);
+                if (diff > allowed_diff) {
+                    panic!(
+                        "{} and {} are not close enough (allowed difference was {}... found {})",
+                        left_val, right_val, allowed_diff, diff
+                    );
                 }
             }
         }
@@ -140,19 +138,21 @@ macro_rules! assert_close {
     ($left:expr, $right:expr ) => {
         match ($left, $right) {
             (left_val, right_val) => {
-                let allowed_diff : f64 = 1e-6;
+                let allowed_diff: f64 = 1e-6;
                 let diff = (left_val as f64 - right_val as f64).abs();
-                if (diff > allowed_diff) {                                        
-                    panic!("{} and {} are not close enough (allowed difference was {}... found {})", left_val, right_val, allowed_diff, diff);
+                if (diff > allowed_diff) {
+                    panic!(
+                        "{} and {} are not close enough (allowed difference was {}... found {})",
+                        left_val, right_val, allowed_diff, diff
+                    );
                 }
             }
         }
     };
 }
 
-
 /// Asserts whether two numbers are close enough
-/// 
+///
 /// # Examples
 /// ```
 /// use validate::assert_not_close;
@@ -165,8 +165,11 @@ macro_rules! assert_not_close {
         match ($left, $right, $allowed_diff) {
             (left_val, right_val, allowed_diff) => {
                 let diff = (left_val as f64 - right_val as f64).abs();
-                if (diff < allowed_diff) {                                        
-                    panic!("{} and {} are too close (minimum difference was {}... found {})", left_val, right_val, allowed_diff, diff);
+                if (diff < allowed_diff) {
+                    panic!(
+                        "{} and {} are too close (minimum difference was {}... found {})",
+                        left_val, right_val, allowed_diff, diff
+                    );
                 }
             }
         }
@@ -174,39 +177,37 @@ macro_rules! assert_not_close {
     ($left:expr, $right:expr ) => {
         match ($left, $right) {
             (left_val, right_val) => {
-                let allowed_diff : f64 = 1e-6;
-                let diff = (left_val as f64 - right_val as f64).abs();                
-                if (diff < allowed_diff) {                                        
-                    panic!("{} and {} are too close (minimum difference was {}... found {})", left_val, right_val, allowed_diff, diff);
+                let allowed_diff: f64 = 1e-6;
+                let diff = (left_val as f64 - right_val as f64).abs();
+                if (diff < allowed_diff) {
+                    panic!(
+                        "{} and {} are too close (minimum difference was {}... found {})",
+                        left_val, right_val, allowed_diff, diff
+                    );
                 }
             }
         }
     };
 }
 
-
-
-/// This structure holds the validations to be ran, runs them, 
+/// This structure holds the validations to be ran, runs them,
 /// and writes the results into a Markdown report.
 pub struct Validations {
-    
     /// The validations to run
     validations: Vec<Box<dyn Validate>>,
 
     /// The file in which the report will be written
-    target_file: &'static str, 
+    target_file: &'static str,
 
     /// The location of the data that will be used by the report (e.g., images)
     report_data_dir: &'static str,
 }
 
 impl Validations {
-
-
-    /// Creates a new `Validator` that will write a report on `target_file` and put the 
+    /// Creates a new `Validator` that will write a report on `target_file` and put the
     /// supporting data on `report_data_dir`
-    pub fn new(target_file: &'static str, report_data_dir: &'static str) -> Self {        
-        Self{
+    pub fn new(target_file: &'static str, report_data_dir: &'static str) -> Self {
+        Self {
             validations: Vec::new(),
             target_file,
             report_data_dir,
@@ -214,81 +215,72 @@ impl Validations {
     }
 
     /// Adds a new validation to the `Validator`
-    pub fn push(&mut self, v: Box<dyn Validate>){
+    pub fn push(&mut self, v: Box<dyn Validate>) {
         self.validations.push(v)
     }
 
     /// Given a filename thaat should be stored into as support data, this
-    /// function returns path to that file inside of the directory containing 
+    /// function returns path to that file inside of the directory containing
     /// supporting data
-    pub fn get_support_path(&self, filename: &'static str)->String{
-        return format!("{}/{}", self.report_data_dir, filename)
+    pub fn get_support_path(&self, filename: &'static str) -> String {
+        return format!("{}/{}", self.report_data_dir, filename);
     }
 
     /// Runs the validations, writes the report and fails the task if necessary
-    pub fn validate(&self)->Result<(),String>{
+    pub fn validate(&self) -> Result<(), String> {
         let mut md = File::create(self.target_file).unwrap();
         let mut errors = Vec::new();
-        for v in self.validations.iter(){
+        for v in self.validations.iter() {
             md.write_all(b"\n\n").unwrap();
-            if let Err(msg) = v.validate(&mut md){
+            if let Err(msg) = v.validate(&mut md) {
                 errors.push(msg);
-            }            
+            }
         }
-        if errors.is_empty(){
-            return Ok(())
-        }else{
-            for e in errors{
-                eprintln!("{}",e);                
+        if errors.is_empty() {
+            return Ok(());
+        } else {
+            for e in errors {
+                eprintln!("{}", e);
             }
             Err("Some validations failed...".to_string())
-        }        
+        }
     }
 }
 
-
-/// The main trait of this crate. All validator modules need 
+/// The main trait of this crate. All validator modules need
 /// to comply with this trait.
 pub trait Validate {
-
-    /// Runs a validation procedure, returning an error message if 
-    /// the validation failed. 
-    /// 
-    /// Whether it fails or not, 
+    /// Runs a validation procedure, returning an error message if
+    /// the validation failed.
+    ///
+    /// Whether it fails or not,
     /// it should write the results of the validations into `file`
     /// so a full report is written.
-    fn validate(&self, file: &mut File)->Result<(),String>;
+    fn validate(&self, file: &mut File) -> Result<(), String>;
 }
 
-
-
 /// Reads a number of columns from a csv
-pub fn from_csv(path: & str, cols: &[usize])->Vec<Vec<f64>>{
+pub fn from_csv(path: &str, cols: &[usize]) -> Vec<Vec<f64>> {
     let reader = File::open(path).unwrap();
-    let mut rdr = csv::Reader::from_reader(reader);        
-    
-    let mut ret : Vec<Vec<f64>> = Vec::with_capacity(cols.len());
-    for _ in cols{
+    let mut rdr = csv::Reader::from_reader(reader);
+
+    let mut ret: Vec<Vec<f64>> = Vec::with_capacity(cols.len());
+    for _ in cols {
         ret.push(Vec::new());
     }
 
-    for record in rdr.records(){
-        let data = record.unwrap();            
-        
-        for (i,col) in cols.iter().enumerate(){
+    for record in rdr.records() {
+        let data = record.unwrap();
+
+        for (i, col) in cols.iter().enumerate() {
             let v = &data[*col].trim();
             let v = v.parse::<f64>().unwrap();
             ret[i].push(v);
         }
-        
     }
 
     ret
 }
-
-
-
-
 
 #[cfg(test)]
 mod tests {
@@ -301,13 +293,12 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_assert_close_fail() {
-        assert_close!(1., 2., 0.2);        
+        assert_close!(1., 2., 0.2);
     }
 
     #[test]
     #[should_panic]
-    fn test_assert_not_close_fail(){
+    fn test_assert_not_close_fail() {
         assert_not_close!(1., 1., 0.1);
     }
-   
 }
--- src/time_series.rs
+++ src/time_series.rs
@@ -18,41 +18,40 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-use poloto::prelude::*;
 use crate::Validate;
+use poloto::prelude::*;
 use std::{fs::File, io::Write};
 
 /// Validates a time series based on Mean Bias Error and Root Mean Squared Error
 #[derive(Default, Clone)]
 pub struct SeriesValidator {
-    
     /// The maximum allowed Mean Bias Error
     pub allowed_mean_bias_error: Option<f64>,
-    
+
     /// The maximum allowed Root Mean Squared Error
     pub allowed_root_mean_squared_error: Option<f64>,
-    
+
     /// The units in the y axis of the chart
     pub y_units: Option<&'static str>,
-    
+
     /// The label in the y axis of the chart
     pub y_label: Option<&'static str>,
-    
+
     /// The units in the x axis of the chart
     pub x_units: Option<&'static str>,
-    
+
     /// The label in the x axis of the chart
     pub x_label: Option<&'static str>,
 
     /// The name of the series caled `expected`
     pub expected_name: &'static str,
-    
+
     /// The time series containing the expected values
     pub expected: Vec<f64>,
 
     /// The name of the `found` time series
     pub found_name: &'static str,
-    
+
     /// The time series containing the found values
     pub found: Vec<f64>,
 
@@ -61,58 +60,103 @@ pub struct SeriesValidator {
 
     /// The title of the section in the Markdown
     pub title: &'static str,
-    
 }
 
-impl Validate for SeriesValidator{
-    
-    fn validate(&self, file: &mut File)->Result<(),String>{
-        if self.expected.len() != self.found.len(){
-            return Err(format!("Series to compare of equal length. expected.len() = {}, found.len() = {}", self.expected.len(), self.found.len()));
+impl Validate for SeriesValidator {
+    fn validate(&self, file: &mut File) -> Result<(), String> {
+
+        let mut err_msg = "".to_string();
+        
+        
+        if self.expected.len() != self.found.len() {
+            return Err(format!(
+                "Series to compare of equal length. expected.len() = {}, found.len() = {}",
+                self.expected.len(),
+                self.found.len()
+            ));
         }
         let n = self.expected.len() as f64;
         let num = self.expected.len();
-        let bias_error : f64 = self.expected.iter().zip(self.found.iter()).map(|(x,y)| *y - *x).sum();
-        let mean_bias_error = bias_error/n;
-        let squared_error : f64 = self.expected.iter().zip(self.found.iter()).map(|(x, y)| (*y - *x).powi(2)).sum();
+        // Process Mean Bias Error
+        let bias_error: f64 = self
+        .expected
+        .iter()
+        .zip(self.found.iter())
+        .map(|(x, y)| *y - *x)
+        .sum();
+        let mean_bias_error = bias_error / n;
+        let mut mbe_msg = format!("Root Mean Squared Error: {:.2}", mean_bias_error);
+
+        // Process Root Mean Squared Error
+        let squared_error: f64 = self
+            .expected
+            .iter()
+            .zip(self.found.iter())
+            .map(|(x, y)| (*y - *x).powi(2))
+            .sum();
         let root_mean_squared_error = (squared_error / n).sqrt();
-        if let Some(allowed_mean_bias_error) = self.allowed_mean_bias_error{
-            if mean_bias_error > allowed_mean_bias_error{
-                return Err(format!("Mean Bias Error is {}, which is greater than the allowed value of {}", mean_bias_error, allowed_mean_bias_error))
+        let mut rmse_msg = format!("* Mean Bias Error: {:.2}", root_mean_squared_error);
+
+        // Check compliance
+        if let Some(allowed_mean_bias_error) = self.allowed_mean_bias_error {
+            if mean_bias_error.abs() > allowed_mean_bias_error {
+                err_msg = format!(
+                    "{}\nAt {}: Mean Bias Error is {}, which is greater than the allowed value of {}",
+                    err_msg, self.title, mean_bias_error, allowed_mean_bias_error
+                );
+                mbe_msg = format!("{} | **Failed!**", mbe_msg)
             }
         }
-        if let Some(allowed_root_mean_squared_error) = self.allowed_root_mean_squared_error{
+        if let Some(allowed_root_mean_squared_error) = self.allowed_root_mean_squared_error {
             if root_mean_squared_error > allowed_root_mean_squared_error {
-                return Err(format!("Mean Root Squared Error is {}, which is greater than the allowed value of {}", root_mean_squared_error, allowed_root_mean_squared_error))
+                err_msg = format!(
+                    "{}\nAt {}: Mean Root Squared Error is {}, which is greater than the allowed value of {}",
+                    err_msg, self.title, root_mean_squared_error, allowed_root_mean_squared_error
+                );
+                rmse_msg = format!("{} | **Failed!**", rmse_msg)
             }
         }
 
-        let line_expected = poloto::build::line(self.expected_name, poloto::range_iter([0.0, n], num).map(|i| [i, self.expected[i as usize] ]));
-        let line_found =    poloto::build::line(self.found_name,    poloto::range_iter([0.0, n], num).map(|i| [i, self.found[i as usize] ]));
+        let line_expected = poloto::build::line(
+            self.expected_name,
+            poloto::range_iter([0.0, n], num).map(|i| [i, self.expected[i as usize]]),
+        );
+        let line_found = poloto::build::line(
+            self.found_name,
+            poloto::range_iter([0.0, n], num).map(|i| [i, self.found[i as usize]]),
+        );
         let m = poloto::build::origin();
         let data = plots!(line_expected, line_found, m);
 
-        let mut x_label = match self.x_label{
-            Some(v)=>v.to_string(),
-            None=>"x".to_string()
+        let mut x_label = match self.x_label {
+            Some(v) => v.to_string(),
+            None => "x".to_string(),
         };
-        if let Some(units) = self.x_units{
+        if let Some(units) = self.x_units {
             x_label = format!("{} ({})", x_label, units);
         }
-        let mut y_label = match self.y_label{
-            Some(v)=>v.to_string(),
-            None=>"y".to_string()
+        let mut y_label = match self.y_label {
+            Some(v) => v.to_string(),
+            None => "y".to_string(),
         };
-        if let Some(units) = self.y_units{
+        if let Some(units) = self.y_units {
             y_label = format!("{} ({})", y_label, units);
         }
         let p = simple_fmt!(data, self.chart_title, x_label, y_label);
 
-        let buf = format!("# {}\n\n * Root Mean Squared Error: {:.2}\n * Mean Bias Error: {:.2}\n\n{}", self.title, root_mean_squared_error, mean_bias_error, poloto::disp(|w| p.simple_theme(w)));
+        let buf = format!(
+            "# {}\n\n * {}\n {}\n\n{}",
+            self.title,
+            rmse_msg,
+            mbe_msg,
+            poloto::disp(|w| p.simple_theme(w))
+        );
         file.write_all(buf.as_bytes()).unwrap();
-        
 
-        Ok(())
-
+        if err_msg.len() > 0 {
+            Err(err_msg)
+        }else{
+            Ok(())
+        }
     }
 }
