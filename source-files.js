352af0814691e7b531d311b08505b41af30313f0
germolinal <germolinal@gmail.com>
2022-07-03 11:25:50 +0000

    Deploying to gh-pages from @ SIMPLE-BuildingSimulation/validate@fd3795401f4c6e20f38410f320de795b3364de0b 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var sourcesIndex = {};
-sourcesIndex["validate"] = {"name":"","files":["lib.rs","scatter.rs","time_series.rs","validator_wrapper.rs"]};
+sourcesIndex["validate"] = {"name":"","files":["lib.rs","scatter.rs","stats.rs","time_series.rs","validator_wrapper.rs"]};
 createSourceSidebar();
