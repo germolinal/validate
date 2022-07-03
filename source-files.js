90c66bc7bffe60d9aace21e9f54defc97ac567ea
germolinal <germolinal@gmail.com>
2022-07-03 02:20:45 +0000

    Deploying to gh-pages from @ SIMPLE-BuildingSimulation/validate@17f304f633839ab54b9ecb7d6d24ffc1b1688278 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["validate"] = {"name":"","files":["lib.rs","time_series.rs","validator_wrapper.rs"]};
+var sourcesIndex = {};
+sourcesIndex["validate"] = {"name":"","files":["lib.rs","scatter.rs","time_series.rs","validator_wrapper.rs"]};
 createSourceSidebar();
---
+++ validate/scatter/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ScatterValidator","Validates a time series based on Mean Bias Error and Root Mean Squared Error"]]});
